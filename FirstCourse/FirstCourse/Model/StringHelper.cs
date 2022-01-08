8f736e70eef4a5cac8fc518a680de3db251deb6a
cloverfeng <clover.feng@tpisoftware.com>
2022-01-08 15:38:36 +0800

    TDDCoruse 建立

---
+++ FirstCourse/FirstCourse/Model/StringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FirstCourse.Model
+{
+    public class StringHelper
+    {
+        public static string GetCanonicalForm(string str) 
+        {
+            if (str == null) throw new ArgumentException("worng string");
+
+            return str.Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.ToUpper())
+                .OrderBy(s => s)
+                .Aggregate("",(x, y) => x + " " + y)
+                .Trim();
+        }
+    }
+}
---
+++ FirstCourse/FirstCourse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FirstCourse.Model;
+
+namespace FirstCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var empty = StringHelper.GetCanonicalForm(" ");
+            Console.WriteLine(empty == "");
+
+            empty = StringHelper.GetCanonicalForm("     ");
+            Console.WriteLine(empty == "");
+
+            empty = StringHelper.GetCanonicalForm("          ");
+            Console.WriteLine(empty == "");
+
+            Console.WriteLine(StringHelper.GetCanonicalForm("  Hello    World!"));
+            Console.WriteLine(StringHelper.GetCanonicalForm("     Hello       World!   "));
+            Console.WriteLine(StringHelper.GetCanonicalForm("Hello World!"));
+            Console.WriteLine(StringHelper.GetCanonicalForm("World! Hello"));
+        }
+    }
+}
