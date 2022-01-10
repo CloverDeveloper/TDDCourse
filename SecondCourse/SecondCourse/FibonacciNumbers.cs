caf1386d8b2e191196bbd634df2656887d3a5167
cloverfeng <clover.feng@tpisoftware.com>
2022-01-10 22:14:21 +0800

    建立第二單元測試專案

---
+++ SecondCourse/SecondCourse/FibonacciNumbers.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecondCourse
+{
+    [TestClass]
+    public class FibonacciNumbers
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
