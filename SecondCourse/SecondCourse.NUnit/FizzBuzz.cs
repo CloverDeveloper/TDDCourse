4a317854371119d16e774cfe81e316f339d4185b
cloverfeng <clover.feng@tpisoftware.com>
2022-01-11 22:47:48 +0800

    FizzBuzz測試建立

---
+++ SecondCourse/SecondCourse.NUnit/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondCourse.NUnit
+{
+    [TestFixture]
+    public class FizzBuzz
+    {
+        [TestCase("Fizz",3)]
+        [TestCase("Buzz", 5)]
+        [TestCase("", 7)]
+        [TestCase("FizzBuzz", 15)]
+        public void FizzBuzzTests(string expect,int testVal) 
+        {
+            Assert.AreEqual(expect, RunFizzBuzz(testVal));
+        }
+
+        private string RunFizzBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return "";
+        }
+    }
+}
